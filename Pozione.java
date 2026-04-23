3c07664c0ffaebb7a258a18c7b0cf4bd67b911fb
Niki151 <nicholas.corazza@allievi.bearzi.it>
2026-04-23 16:26:03 +0200

    Update Pozione.java

--- Pozione.java
+++ Pozione.java
@@ -1,13 +1,16 @@
 public class Pozione extends Elemento {
+
     public int effettoCura;
     public int effettoVeleno;
     public String buffStatistiche;
+    public boolean consumata;
 
     public Pozione(String nome, String descrizione, int effettoCura, int effettoVeleno, String buffStatistiche) {
         super(nome, descrizione);
         this.effettoCura = effettoCura;
         this.effettoVeleno = effettoVeleno;
         this.buffStatistiche = buffStatistiche;
+        this.consumata = false;
     }
 
     public int getEffettoCura() {
@@ -34,12 +37,46 @@ public class Pozione extends Elemento {
         this.buffStatistiche = buffStatistiche;
     }
 
-    public void raccogli(){
-        System.out.println("Hai raccolto la pozione: " + nome);
+    public boolean isConsumata() {
+        return consumata;
+    }
+
+    public void usa(Giocatore g) {
+        if (consumata) {
+            System.out.println(getNome() + " has already been consumed.");
+            return;
+        }
+
+        if (effettoCura > 0) {
+            System.out.println("The potion heals " + effettoCura + " HP.");
+            g.setVita(g.getVita() + effettoCura);
+        }
+
+        if (effettoVeleno > 0) {
+            System.out.println("The potion deals " + effettoVeleno + " damage to the player.");
+            g.setVita(g.getVita() - effettoVeleno);
+        }
+
+        if (buffStatistiche != null && !buffStatistiche.isEmpty()) {
+            System.out.println("Special effect: " + buffStatistiche);
+        }
+
+        consumata = true;
+        System.out.println(getNome() + " has been consumed.");
     }
 
     @Override
-    public String stampaDescrizione(){
-        return "pozione: " + nome + ", " + descrizione + ", Cura: " + effettoCura + ", Veleno: " + effettoVeleno + ", Buff: " + buffStatistiche;
+    public void raccogli() {
+        System.out.println("You picked up the potion: " + getNome());
+    }
+
+    @Override
+    public String stampaDescrizione() {
+        return "Potion: " + getNome() + ", Description: " + getDescrizione() + ", Heal: " + effettoCura + ", Poison: " + effettoVeleno + ", Buff: " + buffStatistiche + ", Consumed: " + consumata;
+    }
+
+    @Override
+    public String toString() {
+        return stampaDescrizione();
     }
 }
