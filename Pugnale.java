23b75d58f37e5e6c847bad78d0e77d5144e1aae4
Niki151 <nicholas.corazza@allievi.bearzi.it>
2026-04-23 16:15:58 +0200

    Update Pugnale.java

--- Pugnale.java
+++ Pugnale.java
@@ -1,30 +1,62 @@
-public class Pugnale extends Arma{
+public class Pugnale extends Arma {
 
-    public Pugnale(String nome, String descrizione, int danno, int raggioAzione, int usura) {
-        super("Pugnale", "Molto forte negli scontri ravvicinati", 5, 1, 0 );
+    private double probabilitaCritico = 0.30;
+    private int bonusAffilatura = 0;
+
+    public Pugnale() {
+        super("Dagger", "A fast and deadly close‑range weapon ideal for quick strikes", 5, 1, 0);
     }
 
-    public int getDanno() {
-        return danno;
+    public boolean colpoCritico() {
+        return Math.random() < probabilitaCritico;
     }
 
-    public String getDescrizione() {
-        return descrizione;
+    public void affila() {
+        bonusAffilatura++;
+        setDanno(getDanno() + 1);
+        System.out.println(getNome() + " has been sharpened. Damage increased to " + getDanno());
     }
 
-    public String getNome() {
-        return nome;
+    public void backstab(Nemico n) {
+        if (eRotta()) {
+            System.out.println(getNome() + " is too damaged to perform a backstab.");
+            return;
+        }
+        int danno = (getDanno() + bonusAffilatura) * 3;
+        System.out.println("Backstab! The Dagger deals " + danno + " massive damage.");
+        n.setVita(n.getVita() - danno);
+        setUsura(getUsura() + 15);
+        if (eRotta()) {
+            System.out.println(getNome() + " broke after the backstab!");
+        }
     }
 
-    public int getRaggioAzione() {
-        return raggioAzione;
+    @Override
+    public void usa(Nemico n) {
+        if (eRotta()) {
+            System.out.println(getNome() + " is broken and cannot be used.");
+            return;
+        }
+        int dannoTotale = getDanno() + bonusAffilatura;
+        if (colpoCritico()) {
+            dannoTotale *= 2;
+            System.out.println("Critical strike! Damage doubled.");
+        }
+        System.out.println("You stab " + n.getNome() + " with the Dagger dealing " + dannoTotale + " damage.");
+        n.setVita(n.getVita() - dannoTotale);
+        setUsura(getUsura() + 6);
+        if (eRotta()) {
+            System.out.println(getNome() + " broke!");
+        }
     }
 
-    public int getUsura() {
-        return usura;
+    @Override
+    public String stampaDescrizione() {
+        return getNome() + ", Description: " + getDescrizione() + ", Damage: " + getDanno() + ", Range: " + getRaggioAzione() + ", Wear: " + getUsura() + ", Sharpen Bonus: " + bonusAffilatura + ", Crit Chance: " + (int)(probabilitaCritico * 100) + "%";
     }
-    
-    public String stampaDescrizione(){
-        return nome+", Descrizione: "+descrizione+", Danno: "+danno+", Raggio di Azione: "+raggioAzione+", Usura: "+usura; 
+
+    @Override
+    public String toString() {
+        return stampaDescrizione();
     }
 }
