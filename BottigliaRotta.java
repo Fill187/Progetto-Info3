07d2dc35b70c44efddde4974c0bab1fdf201ac2a
Niki151 <nicholas.corazza@allievi.bearzi.it>
2026-04-23 16:09:57 +0200

    Update BottigliaRotta.java

--- BottigliaRotta.java
+++ BottigliaRotta.java
@@ -1,30 +1,41 @@
-public class BottigliaRotta extends Arma{
+public class BottigliaRotta extends Arma {
 
-    public BottigliaRotta(String nome, String descrizione, int danno, int raggioAzione, int usura) {
-        super("Bottiglia Rotta", "Viene lanciata contro gli avversari", 4, 5, 0);
+    private double probabilitaSanguinamento = 0.30;
+
+    public BottigliaRotta() {
+        super("Broken Bottle", "A shattered glass bottle with sharp edges capable of causing bleeding", 4, 5, 0);
     }
 
-    public int getDanno() {
-        return danno;
+    public void sanguinamento(Nemico n) {
+        if (Math.random() < probabilitaSanguinamento) {
+            System.out.println("The Broken Bottle causes bleeding! The enemy loses 3 extra HP.");
+            n.setVita(n.getVita() - 3);
+        }
     }
 
-    public String getDescrizione() {
-        return descrizione;
+    @Override
+    public void usa(Nemico n) {
+        if (eRotta()) {
+            System.out.println(getNome() + " is too damaged to be used.");
+            return;
+        }
+        int dannoTotale = getDanno();
+        System.out.println("You slash " + n.getNome() + " with the Broken Bottle dealing " + dannoTotale + " damage.");
+        n.setVita(n.getVita() - dannoTotale);
+        sanguinamento(n);
+        setUsura(getUsura() + 12);
+        if (eRotta()) {
+            System.out.println(getNome() + " shattered completely!");
+        }
     }
 
-    public String getNome() {
-        return nome;
+    @Override
+    public String stampaDescrizione() {
+        return getNome() + ", Description: " + getDescrizione() + ", Damage: " + getDanno() + ", Range: " + getRaggioAzione() + ", Wear: " + getUsura() + ", Bleed Chance: " + (int)(probabilitaSanguinamento * 100) + "%";
     }
 
-    public int getRaggioAzione() {
-        return raggioAzione;
-    }
-
-    public int getUsura() {
-        return usura;
-    }
-    
-    public String stampaDescrizione(){
-        return nome+", Descrizione: "+descrizione+", Danno: "+danno+", Raggio di Azione: "+raggioAzione+", Usura: "+usura; 
+    @Override
+    public String toString() {
+        return stampaDescrizione();
     }
 }
